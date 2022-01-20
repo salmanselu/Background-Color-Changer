eed362888c5396c9b5d328dd494500dd6871ebdf
=Salman nazeer <salmanselu2002@gmail.com>
2022-01-20 09:45:17 +0530

    Add files

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+
+function changeColor(){
+    r = Math.floor(Math.random() * 10)
+    g = Math.floor(Math.random() * 10)
+    b = Math.floor(Math.random() * 10)
+    document.body.style.backgroundColor = "#"+r+g+b;
+}
